8982717e0b9eef7279c4d5bf43123e7e299ee7b1
Sam-S-Pina <lesmak.ss@gmail.com>
2022-03-28 13:04:25 -0300

    @Sam-Pina [TASK: CREATE Initial Components]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'menu', pathMatch: 'full'},
+  {path:'sobre', component: SobreComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'rodape', component: RodapeComponent},
+  {path:'entrar', component: EntrarComponent},
+  {path:'cadastrar', component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
