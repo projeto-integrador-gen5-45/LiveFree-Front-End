466d5747a1a4e07007e6ee3d9e3d186e4efba463
Marco Alexandre <marcoalexandre554@gmail.com>
2022-04-01 10:59:59 -0300

    @MzAlexandre [TASK: ADD service and model]

---
+++ src/app/model/Servico.ts
@@ -0,0 +1,10 @@
+export class Servico{
+
+    public id: number
+    public descricao: string
+    public preco: number
+    public avaliacao: number
+    public foto: string
+
+    //importação/ relacionamento
+}
